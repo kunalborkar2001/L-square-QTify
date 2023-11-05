35c449810a8fd700b744c87252313be874388d5e
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-11-05 20:35:12 +0530

    Navigation Bar added with responsiveness

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
    <div>
-    Kunal
+    <Navbar />
    </div>
   );
 }
