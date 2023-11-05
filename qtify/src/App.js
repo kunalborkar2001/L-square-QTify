01a790c415e03227ef49bed260882fbc526db9e2
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-11-05 21:01:22 +0530

    Hero Image added

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,12 +1,14 @@
 
 
 import './App.css';
+import HeroImage from './Components/HeroImage/HeroImage';
 import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
    <div>
     <Navbar />
+    <HeroImage />
    </div>
   );
 }
