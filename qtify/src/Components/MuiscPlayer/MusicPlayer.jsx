b264a2d7d1edb1e77f0c0a5991ed3d5cb6edc12a
Kunal Borkar <137871453+kunalborkar2001@users.noreply.github.com>
2023-11-09 13:43:30 +0530

    Added Audio Slider Smoothness

--- qtify/src/Components/MuiscPlayer/MusicPlayer.jsx
+++ qtify/src/Components/MuiscPlayer/MusicPlayer.jsx
@@ -136,7 +136,29 @@ const MusicPlayer = () => {
                         audioRef.current.currentTime = value;
                     }}
                     sx={{
+                        color: theme.palette.mode === 'dark' ? '#fff' : 'rgba(0,0,0,0.87)',
                         height: 4,
+                        '& .MuiSlider-thumb': {
+                            width: 8,
+                            height: 8,
+                            transition: '0.6s cubic-bezier(.47,1.64,.41,1.1)',
+                            '&:before': {
+                                boxShadow: '0 2px 12px 0 rgba(0,0,0,0.4)',
+                            },
+                            '&:hover, &.Mui-focusVisible': {
+                                boxShadow: `0px 0px 0px 8px ${theme.palette.mode === 'dark'
+                                        ? 'rgb(255 255 255 / 16%)'
+                                        : 'rgb(0 0 0 / 16%)'
+                                    }`,
+                            },
+                            '&.Mui-active': {
+                                width: 20,
+                                height: 20,
+                            },
+                        },
+                        '& .MuiSlider-rail': {
+                            opacity: 0.28,
+                        },
                     }}
                 />
                 <Box
